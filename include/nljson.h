f55162098f3ee230c9e9435b729b6e560dc1fd42
Erik Stromdahl <erik.stromdahl@gmail.com>
2016-06-19 20:30:49 +0200

    Improved API documentation
    Made a few clarifications in the function descriptions
    and corrected a few errors (mismatches between functions
    and prototypes).
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

--- include/nljson.h
+++ include/nljson.h
@@ -19,8 +19,8 @@
 #ifndef _NLJSON_H_
 #define _NLJSON_H_
 
+#include <stddef.h>
 #include <stdint.h>
-#include <stdio.h>
 
 #define NLJSON_ERR_STR_LEN (256)
 
@@ -36,7 +36,7 @@
 typedef struct _nljson nljson_t;
 
 /**
- * Structure used to describe an error that has occured during
+ * Structure used to describe an error that has occurred during
  * any operation (encoding, decoding or initialization).
  */
 struct nljson_error {
@@ -60,6 +60,9 @@ struct nljson_error {
  * These functions will allocate the handle and create the nl attribute
  * policy from the JSON input. The different init functions will read
  * the JSON policy in different ways.
+ *
+ * The allocated handle can be used with the encode family of functions
+ * in order to control the behavior of the encoding.
  */
 
 /**
@@ -67,28 +70,24 @@ struct nljson_error {
  *
  * @arg hdl               Handle that will be allocated
  *
- * @arg json_format_flags Flags for the JSON input decoding of the
- *                        nla policy.
+ * @arg json_format_flags Flags for the JSON decoding of the nla policy.
  *                        Passed directly to the jansson library.
  *                        See jansson documentation for more info.
  *
  * @arg nljson_flags      Flags for the JSON encoding of the nla stream.
  *                        See description of each flag for more info.
+ *                        These flags will be used by the encode functions
+ *                        when called with the handle obtained from this
+ *                        function.
  *
  * @arg policy_json       The nla policy definition string.
  *                        Must be a valid JSON string.
- *                        If NULL, no policy will be created and
- *                        all encoded attributes will have the value
- *                        set to UNKNOWN_ATTR_<attr_type> and data_type
- *                        set to NLA_UNSPEC.
- *                        The same thing applies to any attribute not present
- *                        in the policy.
- *                        This behavior can be overridden if
- *                        NLJSON_FLAG_SKIP_UNKNOWN_ATTRS is set.
- *                        In this case, all unknown attributes
- *                        (attributes not present in the policy) will be
- *                        skipped. A consequence of this is that there
- *                        will be no output at all if policy_json is NULL
+ *                        If NULL, the handle will not contain any policy.
+ *                        The policy will be used by the encode functions
+ *                        when called with the handle obtained from this
+ *                        function.
+ *                        See description of encode functions for more
+ *                        details.
  *
  * @arg error             Error output. The struct must be allocated by the
  *                        caller.
@@ -108,28 +107,24 @@ int nljson_init(nljson_t **hdl,
  *
  * @arg hdl               Handle that will be allocated
  *
- * @arg json_format_flags Flags for the JSON input decoding of the
- *                        nla policy.
+ * @arg json_format_flags Flags for the JSON decoding of the nla policy.
  *                        Passed directly to the jansson library.
  *                        See jansson documentation for more info.
  *
  * @arg nljson_flags      Flags for the JSON encoding of the nla stream.
  *                        See description of each flag for more info.
+ *                        These flags will be used by the encode functions
+ *                        when called with the handle obtained from this
+ *                        function.
  *
- * @arg policy_file       The path to a nla policy definition file.
- *                        The path must point to a valid JSON file.
- *                        If NULL, no policy will be created and
- *                        all encoded attributes will have the value
- *                        set to UNKNOWN_ATTR_<attr_type> and data_type
- *                        set to NLA_UNSPEC.
- *                        The same thing applies to any attribute not present
- *                        in the policy.
- *                        This behavior can be overridden if
- *                        NLJSON_FLAG_SKIP_UNKNOWN_ATTRS is set.
- *                        In this case, all unknown attributes
- *                        (attributes not present in the policy) will be
- *                        skipped. A consequence of this is that there
- *                        will be no output at all if policy_json is NULL
+ * @arg policy_json       The nla policy definition string.
+ *                        Must be a valid JSON string.
+ *                        The policy will be used by the encode functions
+ *                        when called with the handle obtained from this
+ *                        function.
+ *                        If NULL, the handle will not contain any policy.
+ *                        See description of encode functions for more
+ *                        details.
  *
  * @arg error             Error output. The struct must be allocated by the
  *                        caller.
@@ -150,13 +145,15 @@ int nljson_init_file(nljson_t **hdl,
  *
  * @arg hdl               Handle that will be allocated
  *
- * @arg json_format_flags Flags for the JSON input decoding of the
- *                        nla policy.
+ * @arg json_format_flags Flags for the JSON decoding of the nla policy.
  *                        Passed directly to the jansson library.
  *                        See jansson documentation for more info.
  *
  * @arg nljson_flags      Flags for the JSON encoding of the nla stream.
  *                        See description of each flag for more info.
+ *                        These flags will be used by the encode functions
+ *                        when called with the handle obtained from this
+ *                        function.
  *
  * @arg read_policy_cb    will be called continuously by nljson_init
  *                        when reading the JSON encoded nla policy
@@ -164,18 +161,10 @@ int nljson_init_file(nljson_t **hdl,
  *                        The callback is expected to fill buf with
  *                        at most size bytes and return the number
  *                        of bytes written.
- *                        If NULL, no policy will be created and
- *                        all encoded attributes will have the value
- *                        set to UNKNOWN_ATTR_<attr_type> and data_type
- *                        set to NLA_UNSPEC.
- *                        The same thing applies to any attribute not present
- *                        in the policy.
- *                        This behavior can be overridden if
- *                        NLJSON_FLAG_SKIP_UNKNOWN_ATTRS is set.
- *                        In this case, all unknown attributes
- *                        (attributes not present in the policy) will be
- *                        skipped. A consequence of this is that there
- *                        will be no output at all if policy_json is NULL
+ *                        If NULL, no policy definition will be read and
+ *                        the handle will not contain any policy.
+ *                        See description of encode functions for more
+ *                        details.
  *
  * @arg cb_data           pointer that will passed to read_policy_cb.
  *
@@ -204,13 +193,21 @@ void nljson_deinit(nljson_t **hdl);
 /**
  * Encode family of functions.
  *
- * These functions will encode a stream of netlink attribute (nla_stream)
- * into a JSON encode string using the nla policy that was created in the
+ * These functions will encode a stream of netlink attributes (nla_stream)
+ * into a JSON encoded string using the nla policy that was created in the
  * init step.
  *
  * If the handle (hdl) is NULL (no nljson_init function has been called)
- * all decoded attributes will have the value set to UNKNOWN_ATTR_<attr_type>
- * and data_type set to NLA_UNSPEC
+ * or if the handle does not contain any policy (no policy was provided
+ * to nljson_init) all encoded attributes will have the value set to
+ * UNKNOWN_ATTR_<attr_type> and data_type set to NLA_UNSPEC.
+ * <attr_type> is the attr_type value of the attribute.
+ *
+ * This behavior is overridden if NLJSON_FLAG_SKIP_UNKNOWN_ATTRS was
+ * provided to any of the nljson_init functions.
+ * In this case, all unknown attributes (attributes not present in the
+ * policy) will be skipped. A consequence of this is that there will be no
+ * output at all if the handle does not contain a policy.
  */
 
 /**
@@ -219,11 +216,6 @@ void nljson_deinit(nljson_t **hdl);
  *
  * @arg hdl               The nljson handle. Must be allocated by one
  *                        of the init functions.
- *                        The handle holds the nla policy used when
- *                        parsing nla_stream.
- *                        If NULL, all decoded attributes will have the
- *                        value set to UNKNOWN_ATTR_<attr_type> and
- *                        data_type set to NLA_UNSPEC
  *
  * @arg nla_stream        Stream of bytes containing netlink attributes
  *
@@ -240,7 +232,7 @@ void nljson_deinit(nljson_t **hdl);
  * @arg bytes_produced    The number of output bytes produced, i.e. the
  *                        length of the JSON output.
  *
- * @arg json_format_flags Flags for the JSON output formating.
+ * @arg json_format_flags Flags for the JSON output formatting.
  *                        Passed directly to the jansson library.
  *                        See jansson documentation for more info.
  *
@@ -269,11 +261,6 @@ int nljson_encode_nla(nljson_t *hdl,
  *
  * @arg hdl               The nljson handle. Must be allocated by one
  *                        of the init functions.
- *                        The handle holds the nla policy used when
- *                        parsing nla_stream.
- *                        If NULL, all decoded attributes will have the
- *                        value set to UNKNOWN_ATTR_<attr_type> and
- *                        data_type set to NLA_UNSPEC
  *
  * @arg nla_stream        Stream of bytes containing netlink attributes
  *
@@ -285,7 +272,7 @@ int nljson_encode_nla(nljson_t *hdl,
  * @arg bytes_produced    The number of output bytes produced, i.e. the
  *                        length of the JSON output.
  *
- * @arg json_format_flags Flags for the JSON output formating.
+ * @arg json_format_flags Flags for the JSON output formatting.
  *                        Passed directly to the jansson library.
  *                        See jansson documentation for more info.
  *
@@ -310,11 +297,6 @@ char *nljson_encode_nla_alloc(nljson_t *hdl,
  *
  * @arg hdl               The nljson handle. Must be allocated by one
  *                        of the init functions.
- *                        The handle holds the nla policy used when
- *                        parsing nla_stream.
- *                        If NULL, all decoded attributes will have the
- *                        value set to UNKNOWN_ATTR_<attr_type> and
- *                        data_type set to NLA_UNSPEC
  *
  * @arg nla_stream        Stream of bytes containing netlink attributes
  *
@@ -328,7 +310,7 @@ char *nljson_encode_nla_alloc(nljson_t *hdl,
  *
  * @arg cb_data           pointer that will passed to encode_cb.
  *
- * @arg json_format_flags Flags for the JSON output formating.
+ * @arg json_format_flags Flags for the JSON output formatting.
  *                        Passed directly to the jansson library.
  *                        See jansson documentation for more info.
  *
@@ -360,12 +342,9 @@ int nljson_encode_nla_cb(nljson_t *hdl,
 
 /**
  * Decodes a JSON encoded string of nl attributes into the byte stream
- * nla_stream. nla_stream is a continuous stream of nl attributes that
- * can be written directly into an nlmsg.
+ * nla_stream.
  *
  * @arg input             JSON encoded input string.
- *
- * @arg input_len         Length of the input string.
  *
  * @arg nla_stream        Output: stream of bytes containing netlink attributes
  *
@@ -400,11 +379,8 @@ int nljson_decode_nla(const char *input,
  * Similar to nljson_decode_nla but the output buffer is allocated
  * by the function and returned to the caller.
  * The caller is responsible for deallocating the buffer.
- * In case of error, NULL will be returned.
  *
  * @arg input             JSON encoded input string.
- *
- * @arg input_len         Length of the input string.
  *
  * @arg bytes_consumed    The number of bytes read (consumed) from
  *                        input.
@@ -436,13 +412,13 @@ void *nljson_decode_nla_alloc(const char *input,
  *
  * @arg input             JSON encoded input string.
  *
- * @arg input_len         Length of the input string.
- *
  * @arg bytes_consumed    The number of bytes read (consumed) from
  *                        input.
  *
  * @arg decode_cb         will be called continuously when writing
  *                        the decoded nla output stream.
+ *
+ * @arg cb_data           pointer that will passed to decode_cb.
  *
  * @arg json_decode_flags Flags for the JSON input parsing.
  *                        Passed directly to the jansson library.
